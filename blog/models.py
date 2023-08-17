bd68d4c34f36c3b61e35ec3292d06ac10b6e48d4
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2023-08-18 03:24:12 +0600

    database conn , create blog, get blogs, search blog

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from database import Base
 
-class user(Base):
-    __tablename__='users'
-
-    id = Column(Integer, primary_key = True, index=True)
-    username = Column(String(50))
-
+class Blog(Base):
+    __tablename__ = 'blogs'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
