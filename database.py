41d5b5937163b818afdfc65d4685affeaf3d19e0
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2023-08-21 02:08:07 +0600

    SQLITE Update

--- database.py
+++ database.py
@@ -1,27 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from mysql.connector import connect
 
+SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 
-# MySQL Database Configuration
-DATABASE_URL = {
-    "host": "localhost",
-    "port": 3306,
-    "user": "root",
-    "password": "",
-    "database": "fastapi_db"
-}
+engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
 
-engine = create_engine(
-    f"mysql+mysqlconnector://{DATABASE_URL['user']}:{DATABASE_URL['password']}@{DATABASE_URL['host']}:{DATABASE_URL['port']}/{DATABASE_URL['database']}")
-
-# SQLALCHEMY_DATABASE_URL="mysql://root@localhost:3306/fastapi_db"
-
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
 Base = declarative_base()
